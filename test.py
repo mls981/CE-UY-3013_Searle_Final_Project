df8ecbe4a0f19b20bf3545b7712473ad2fc7c2e1
mls981 <73948781+mls981@users.noreply.github.com>
2020-12-22 12:36:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ from source import System
 
 trial1 = System(ReactorType='Batch', order=1, volume=100, flowrate=10, Cin=50, kVal=0.2, tBatch=40)
 print(trial1.Cout_find())
+
+trial2 = System2(ReactorType = 'PFR', order = 1, Cin = 130, kVal = 0.3, removalGoal = 99) 
+print (trial2.tNeeded_find())
