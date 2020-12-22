3c67ba9f0ce36de7b7a33ab85b975ea769149a5c
mls981 <73948781+mls981@users.noreply.github.com>
2020-12-21 19:24:32 -0500

    Update source.py

--- source.py
+++ source.py
@@ -1,7 +1,3 @@
-#IMPORTS
-import math 
-
-
 class System:
     """
     Two cases:
@@ -72,35 +68,36 @@ class System:
 
     
 class System2:
-  """
-  If case 2, use this class.
-  """
-  def __init2__(self, ReactorType, order, Cin, kVal, CoutGoal=None, removalGoal=None):
-    self.reactor = ReactorType
-    self.order = order
-    self.removalGoal = removalGoal
-    self.CoutGoal = CoutGoal
-    self.Cin = Cin
-    self.kVal = kVal
+    """
+    If case 2, use this class.
+    """
+    def __init__(self, ReactorType, order, Cin, kVal, removalGoal=None, CoutGoal=None):
+        self.reactor = ReactorType
+        self.order = order
+        self.removalGoal = removalGoal
+        self.CoutGoal = CoutGoal
+        self.Cin = Cin
+        self.kVal = kVal
   
-  def conv_rg_to_CoutGoal(self, self.removalGoal, self.Cin):
-    if self.removalGoal != None:
-     return ((100-self.removalGoal)/100)*self.Cin
-  
-  def tNeeded_find(self, self.Cin, self.kVal, self.ReactorType, self.order, self.CoutGoal): 
-    if self.ReactorType == 'PFR' or 'Batch': # PFR/Batch nested if statements
-      if self.order == 0:
-       tNeeded = (1/self.kVal)*(self.Cin-CoutGoal)
-      if self.order == 1:
-       tNeeded = (1/self.kVal)*(math.log(self.Cin/CoutGoal))
-      if self.order == 2:
-       tNeeded = (1/(self.kVal*self.Cin))*((self.Cin/CoutGoal)-1)
-    elif self.ReactorType == 'CMFR': # CMFR nested if statements
-      if self.order == 0:
-        tNeeded = (CoutGoal/self.kVal)*((self.Cin/CoutGoal)-1)
-      if self.order == 1:
-        tNeeded = (1/self.kVal)*((self.Cin/CoutGoal)-1)
-      if self.order == 2:
-        tNeeded = (1/(self.kVal*CoutGoal))*((self.Cin/CoutGoal)-1)
-  return tNeeded
+    def conv_rg_to_CoutGoal(self, removalGoal, Cin):
+        if self.removalGoal != None:
+            return ((100-self.removalGoal)/100)*self.Cin
+    
+    def tNeeded_find(self, Cin, kVal, ReactorType, order, CoutGoal): 
+        if self.ReactorType == 'PFR' or 'Batch': # PFR/Batch nested if statements
+            if self.order == 0:
+                tNeeded = (1/self.kVal)*(self.Cin-CoutGoal)
+            if self.order == 1:
+                tNeeded = (1/self.kVal)*(math.log(self.Cin/CoutGoal))
+            if self.order == 2:
+                tNeeded = (1/(self.kVal*self.Cin))*((self.Cin/CoutGoal)-1)
+        elif self.ReactorType == 'CMFR': # CMFR nested if statements
+            if self.order == 0:
+                tNeeded = (CoutGoal/self.kVal)*((self.Cin/CoutGoal)-1)
+            if self.order == 1:
+                tNeeded = (1/self.kVal)*((self.Cin/CoutGoal)-1)
+            if self.order == 2:
+                tNeeded = (1/(self.kVal*CoutGoal))*((self.Cin/CoutGoal)-1)
+                
+        return tNeeded
 
