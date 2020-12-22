7712025cffe68bfcd10c30dd62ce7a2b1ab0fcac
mls981 <73948781+mls981@users.noreply.github.com>
2020-12-21 19:21:15 -0500

    Update source.py

--- source.py
+++ source.py
@@ -75,7 +75,7 @@ class System2:
   """
   If case 2, use this class.
   """
-  def __init2__(self, ReactorType, order, removalGoal=None, CoutGoal=None, Cin, kVal):
+  def __init2__(self, ReactorType, order, Cin, kVal, CoutGoal=None, removalGoal=None):
     self.reactor = ReactorType
     self.order = order
     self.removalGoal = removalGoal
@@ -83,13 +83,11 @@ class System2:
     self.Cin = Cin
     self.kVal = kVal
   
-  def conv_rg_to_CoutGoal(removalGoal, Cin):
-    if removalGoal != None:
-     return ((100-removalGoal)/100)*Cin
+  def conv_rg_to_CoutGoal(self, self.removalGoal, self.Cin):
+    if self.removalGoal != None:
+     return ((100-self.removalGoal)/100)*self.Cin
   
-  CoutGoal = conv_rg_to_CoutGoal(removalGoal, Cin)
-  
-  def tNeeded_find(Cin, kVal, ReactorType, order, CoutGoal): 
+  def tNeeded_find(self, self.Cin, self.kVal, self.ReactorType, self.order, self.CoutGoal): 
     if self.ReactorType == 'PFR' or 'Batch': # PFR/Batch nested if statements
       if self.order == 0:
        tNeeded = (1/self.kVal)*(self.Cin-CoutGoal)
@@ -97,7 +95,7 @@ class System2:
        tNeeded = (1/self.kVal)*(math.log(self.Cin/CoutGoal))
       if self.order == 2:
        tNeeded = (1/(self.kVal*self.Cin))*((self.Cin/CoutGoal)-1)
-    if self.ReactorType == 'CMFR': # CMFR nested if statements
+    elif self.ReactorType == 'CMFR': # CMFR nested if statements
       if self.order == 0:
         tNeeded = (CoutGoal/self.kVal)*((self.Cin/CoutGoal)-1)
       if self.order == 1:
