1d5c3159c1b64b4c056da21bd28ca61ddef3e4ed
mls981 <73948781+mls981@users.noreply.github.com>
2020-12-22 21:42:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -10,20 +10,16 @@ print('Percent removal for first trial: ', trial1.pRemoval_find())
 trial2 = System(ReactorType='PFR', order=1, volume=100, flowrate=10, Cin=50, kVal=0.2)
 print('Percent removal for second trial: ',trial2.pRemoval_find())
 
-def compSystem(self):
-# initialize list with results from trial runs using the class 'System'
-    results = []
-    # add results from trials
-    results.append(trial1.pRemoval_find())
-    results.append(trial2.pRemoval_find())
-    if results[0] > results[1]:
-        print('Trial 1 is a more efficient reactor model.')
-     if results[0] < results[1]:
-        print('Trial 2 is a more efficient reactor model.')
-     if results[0] == results[1]:
-        print('Both reactor models have the same percent removal.')
+results = []
+results.append(trial1.pRemoval_find())
+results.append(trial2.pRemoval_find())
 
-comparison = compSystem(results)
+if results[0] > results[1]:
+    print('Trial 1 is a more efficient reactor model.')
+if results[0] < results[1]:
+    print('Trial 2 is a more efficient reactor model.')
+if results[0] == results[1]:
+    print('Both reactor models have the same percent removal.')
 
 # S2trial uses the System2 class to determine the time needed to achieve given removal goal
 # or outlet concentration goal.
