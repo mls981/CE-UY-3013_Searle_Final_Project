4c8783562a64aa907ea87c27f3c6bb97e550a48e
mls981 <73948781+mls981@users.noreply.github.com>
2020-12-22 19:29:19 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,31 @@
 
 from source import System
 
+# 'trial1' and 'trial2,' compare two different reactor models using the System class.
 trial1 = System(ReactorType='Batch', order=1, volume=100, flowrate=10, Cin=50, kVal=0.2, tBatch=40)
-print(trial1.Cout_find())
+print('Percent removal: ', trial1.pRemoval_find())
 
-trial2 = System2(ReactorType = 'PFR', order = 1, Cin = 130, kVal = 0.3, removalGoal = 99) 
-print(trial2.tNeeded_find())
+trial2 = System(ReactorType='PFR', order=1, volume=100, flowrate=10, Cin=50, kVal=0.2)
+print(trial2.pRemoval_find())
+
+# COMPARISON FUNCTION 
+# initialize list with results from above trial runs
+results = []
+# add results from the two trials
+results.append(trial1.pRemoval_find())
+results.append(trial2.pRemoval_find())
+
+def compSystem(results):
+  if results[0] > results[1]:
+    print('Trial 1 is a more efficient reactor model.')
+  if results[0] < results[1]:
+    print('Trial 2 is a more efficient reactor model.')
+  if results[0] == results[1]:
+    print('Both reactor models have the same percent removal.')
+
+comparison = compSystem(results)
+
+# S2trial uses the System2 class to determine the time needed to achieve given removal goal
+# or outlet concentration goal.
+S2trial = System2(ReactorType = 'PFR', order = 1, Cin = 130, kVal = 0.3, removalGoal = 99) 
+print('Time needed to achieve goal: ', round(S2trial.tNeeded_find()), 'minutes')
